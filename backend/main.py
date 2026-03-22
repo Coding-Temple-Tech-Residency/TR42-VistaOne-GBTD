727b5b926056f8280298d23865c2faddffd1e57f
Deepalakshmi Lakshminarasimhan <198509696+deepalakshmil@users.noreply.github.com>
2026-03-22 17:30:06 -0400

    Env Set up and running in local

--- backend/main.py
+++ backend/main.py
@@ -1,39 +1,44 @@
 from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from app.dependencies import get_db
-from app.db.models.users import User
-from app.api import vendors, workorders, workorder_items, invoices, contractors, me
+#from app.dependencies import get_db
+#from app.db.models.users import User
+#from app.api import vendors, workorders, workorder_items, invoices, contractors, me
 
 app = FastAPI()
 
-@app.get("/users")
-def list_users(db: Session = Depends(get_db)):
-    return db.query(User).all()
-from sqlalchemy import create_engine
-# from sqlalchemy.pool import NullPool
-from dotenv import load_dotenv
-import os
+@app.get("/hello-world")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+# @app.get("/users")
+# def list_users(db: Session = Depends(get_db)):
+#     return db.query(User).all()
+# from sqlalchemy import create_engine
+# # from sqlalchemy.pool import NullPool
+# from dotenv import load_dotenv
+# import os
 
 # Load environment variables from .env
-load_dotenv()
+#load_dotenv()
 
 # Fetch variables
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
+# USER = os.getenv("user")
+# PASSWORD = os.getenv("password")
+# HOST = os.getenv("host")
+# PORT = os.getenv("port")
+# DBNAME = os.getenv("dbname")
 
 # Construct the SQLAlchemy connection string
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+# DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 # Create the SQLAlchemy engine
-engine = create_engine(DATABASE_URL)
-try:
-    with engine.connect() as connection:
-        print("Connection successful!")
-except Exception as e:
-    print(f"Failed to connect: {e}")
+# engine = create_engine(DATABASE_URL)
+# try:
+#     with engine.connect() as connection:
+#         print("Connection successful!")
+# except Exception as e:
+#     print(f"Failed to connect: {e}")
     
     
     
@@ -41,9 +46,9 @@ except Exception as e:
     
 
 
-app.include_router(me.router)
-app.include_router(vendors.router)
-app.include_router(workorders.router)
-app.include_router(workorder_items.router)
-app.include_router(invoices.router)
-app.include_router(contractors.router)
+# app.include_router(me.router)
+# app.include_router(vendors.router)
+# app.include_router(workorders.router)
+# app.include_router(workorder_items.router)
+# app.include_router(invoices.router)
+# app.include_router(contractors.router)
